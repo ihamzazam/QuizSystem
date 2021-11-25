6dc32548ad21d272664767ffe0cef236562c7925
ihamzazam <hamzazam00@gmail.com>
2021-11-26 01:23:40 +0800

    comments and slight refactoring

--- src/Main.java
+++ src/Main.java
@@ -32,7 +32,7 @@ public class Main extends Application {
 	private File myf = new File("./data", "inputdata.txt");
 	private File myf2 = new File("./data", "answers.txt");
 	private int totQues = 0;
-	private int activeQ = 1;
+	private int activeQues = 1;
 	private Label labQuesNo, labQues, labName, labCountry, labID;
 	private ImageView imgQues;
 	private ImageView imgAnsA, imgAnsB, imgAnsC, imgAnsD;
@@ -56,7 +56,7 @@ public class Main extends Application {
 	public void start(Stage mainStage) {
 		mainStage.setTitle("Miss Universe Knowledge Test");
 
-		//rightPanel
+		// setting up the line that divides the quiz screen into two panels 
 		rightPanel = new Rectangle();
 		rightPanel.setWidth(5);
 		rightPanel.setHeight(900);
@@ -64,62 +64,73 @@ public class Main extends Application {
 		rightPanel.setY(0);
 		rightPanel.setFill(Color.BLACK);		
 		
-		// Layouts of components
+		// Layout and styling for the name label 
 		labName = new Label("");
 		labName.setLayoutX(1050);
 		labName.setLayoutY(120);
 		labName.setStyle("-fx-font-size: 20pt;-fx-pref-width: 200px;-fx-font-weight:bold;");
 
+                // Layout and styling for the country label
 		labCountry = new Label("");
 		labCountry.setLayoutX(1050);
 		labCountry.setLayoutY(505);
 		labCountry.setStyle("-fx-font-size: 20pt;-fx-pref-width: 200px;-fx-font-weight:bold;");
 
+                // Layout and styling for the ID label
 		labID = new Label("");
 		labID.setLayoutX(1240);
 		labID.setLayoutY(120);
 		labID.setStyle("-fx-font-size: 20pt;-fx-font-weight:bold;");
 
+                // Layout and styling for the question number label
 		labQuesNo = new Label("");
 		labQuesNo.setLayoutX(50);
 		labQuesNo.setLayoutY(50);
 		labQuesNo.setStyle("-fx-font-size: 18pt;-fx-text-fill:#000000;-fx-font-weight:bold;");
 
+                // Layout and styling for the question label
 		labQues = new Label("");
 		labQues.setLayoutX(50);
 		labQues.setLayoutY(85);
 		labQues.setStyle("-fx-font-size: 20pt;-fx-font-weight:bold;");
 
+                // Layout and styling for the question with image label
 		imgQues = new ImageView();
 		imgQues.setLayoutX(50);
 		imgQues.setLayoutY(75);
 		imgQues.setFitHeight(250);
 		imgQues.setFitWidth(300);
-
+                
+                // Layout and styling for image answer choice A label
 		imgAnsA = new ImageView();
 		imgAnsA.setLayoutX(75);
 		imgAnsA.setLayoutY(75);
 		imgAnsA.setFitHeight(200);
 		imgAnsA.setFitWidth(300);
-
+                
+                
+                // Layout and styling for image answer choice B label
 		imgAnsB = new ImageView();
 		imgAnsB.setLayoutX(75);
 		imgAnsB.setLayoutY(300);
 		imgAnsB.setFitHeight(200);
 		imgAnsB.setFitWidth(300);
-
+                
+                // Layout and styling for image answer choice C label
 		imgAnsC = new ImageView();
 		imgAnsC.setLayoutX(500);
 		imgAnsC.setLayoutY(75);
 		imgAnsC.setFitHeight(200);
 		imgAnsC.setFitWidth(300);
 
+                // Layout and styling for image answer choice D label
 		imgAnsD = new ImageView();
 		imgAnsD.setLayoutX(500);
 		imgAnsD.setLayoutY(300);
 		imgAnsD.setFitHeight(200);
 		imgAnsD.setFitWidth(300);
 
+                // Layout and styling for choice A label
 		labA = new Label("A ");
 		labA.setLayoutX(25);
                 labA.setStyle("-fx-font-size: 20pt;-fx-font-weight:bold;");
@@ -127,6 +138,7 @@ public class Main extends Application {
 		radChoice1.setLayoutX(50);
                 radChoice1.setStyle("-fx-font-size: 12pt;-fx-font-weight:bold;");
                 
+                // Layout and styling for choice B label
 		labB = new Label("B ");
 		labB.setLayoutX(25);
                 labB.setStyle("-fx-font-size: 20pt;-fx-font-weight:bold;");
@@ -134,6 +146,7 @@ public class Main extends Application {
 		radChoice2.setLayoutX(50);
                 radChoice2.setStyle("-fx-font-size: 12pt;-fx-font-weight:bold;");
                 
+                // Layout and styling for choice C label
 		labC = new Label("C ");
 		labC.setLayoutX(25);
                 labC.setStyle("-fx-font-size: 20pt;-fx-font-weight:bold;");
@@ -141,6 +154,7 @@ public class Main extends Application {
 		radChoice3.setLayoutX(50);
                 radChoice3.setStyle("-fx-font-size: 12pt;-fx-font-weight:bold;");
 
+                // Layout and styling for choice D label
 		labD = new Label("D ");
 		labD.setLayoutX(25);
                 labD.setStyle("-fx-font-size: 20pt;-fx-font-weight:bold;");
@@ -154,19 +168,22 @@ public class Main extends Application {
 		radChoice2.setToggleGroup(grpChoices);
 		radChoice3.setToggleGroup(grpChoices);
 		radChoice4.setToggleGroup(grpChoices);
-
+                
+                // Layout and styling for time remaining label
 		timeText = new Label("Time Remaining");
 		timeText.setTextFill(Color.BLACK);
 		timeText.setStyle("-fx-font-size: 15pt;-fx-font-family:sans-serif;-fx-font-weight:BOLD;");
 		timeText.setLayoutX(1120);
 		timeText.setLayoutY(25);
-				
+		
+                // Layout and styling for the normal timer label
 		timerLabel = new Label();
 		timerLabel.setTextFill(Color.BLUE);
 		timerLabel.setStyle("-fx-font-size: 20pt;-fx-font-weight:BOLD;");
 		timerLabel.setLayoutX(1120);
 		timerLabel.setLayoutY(50);
 
+                // Layout and styling for time when less than 10 secs left label
 		timerLabelR = new Label();
 		timerLabelR.setTextFill(Color.RED);
 		timerLabelR.setStyle("-fx-font-size: 20pt;-fx-font-weight:BOLD;");
@@ -196,47 +213,47 @@ public class Main extends Application {
 		btnSubmit.setLayoutY(800);
 		btnSubmit.setStyle("-fx-pref-width: 200px;-fx-pref-height: 40px;-fx-font-size: 15px");
 		
-		// call to read from file method to read questions from the txt file
+		// call to read from file method to read questions from the file "inputdata.txt"
 		readFromFile();
 		radChoice1.setOnAction(e -> {
-			quesList.get(activeQ - 1).setSelected(0, true);
-			quesList.get(activeQ - 1).setSelected(1, false);
-			quesList.get(activeQ - 1).setSelected(2, false);
-			quesList.get(activeQ - 1).setSelected(3, false);
+			quesList.get(activeQues - 1).setSelected(0, true);
+			quesList.get(activeQues - 1).setSelected(1, false);
+			quesList.get(activeQues - 1).setSelected(2, false);
+			quesList.get(activeQues - 1).setSelected(3, false);
 		});
 		radChoice2.setOnAction(e -> {
-			quesList.get(activeQ - 1).setSelected(0, false);
-			quesList.get(activeQ - 1).setSelected(1, true);
-			quesList.get(activeQ - 1).setSelected(2, false);
-			quesList.get(activeQ - 1).setSelected(3, false);
+			quesList.get(activeQues - 1).setSelected(0, false);
+			quesList.get(activeQues - 1).setSelected(1, true);
+			quesList.get(activeQues - 1).setSelected(2, false);
+			quesList.get(activeQues - 1).setSelected(3, false);
 		});
 		radChoice3.setOnAction(e -> {
-			quesList.get(activeQ - 1).setSelected(0, false);
-			quesList.get(activeQ - 1).setSelected(1, false);
-			quesList.get(activeQ - 1).setSelected(2, true);
-			quesList.get(activeQ - 1).setSelected(3, false);
+			quesList.get(activeQues - 1).setSelected(0, false);
+			quesList.get(activeQues - 1).setSelected(1, false);
+			quesList.get(activeQues - 1).setSelected(2, true);
+			quesList.get(activeQues - 1).setSelected(3, false);
 		});
 		radChoice4.setOnAction(e -> {
-			quesList.get(activeQ - 1).setSelected(0, false);
-			quesList.get(activeQ - 1).setSelected(1, false);
-			quesList.get(activeQ - 1).setSelected(2, false);
-			quesList.get(activeQ - 1).setSelected(3, true);
+			quesList.get(activeQues - 1).setSelected(0, false);
+			quesList.get(activeQues - 1).setSelected(1, false);
+			quesList.get(activeQues - 1).setSelected(2, false);
+			quesList.get(activeQues - 1).setSelected(3, true);
 		});
 		if (totQues == 1) {
 			btnNext.setDisable(true);
 		}
 		btnNext.setOnAction(e -> {
-			activeQ++;
+			activeQues++;
 			btnPrev.setDisable(false);
-			if (activeQ == totQues) {
+			if (activeQues == totQues) {
 				btnNext.setDisable(true);
 			}
 			reloadQues();
 		});
 		btnPrev.setOnAction(e -> {
-			activeQ--;
+			activeQues--;
 			btnNext.setDisable(false);
-			if (activeQ == 1) {
+			if (activeQues == 1) {
 				btnPrev.setDisable(true);
 			}
 			reloadQues();
@@ -253,7 +270,7 @@ public class Main extends Application {
 		mainPane = new Pane();
 		mainPane.getChildren().addAll(labName,labCountry,labID,labQuesNo,labQues,paneC,btnNext,btnPrev,btnSubmit,timeText,timerLabel,timerLabelR,rightPanel);
 
-		mainScene = new Scene(mainPane, 1400, 900); // reszie
+		mainScene = new Scene(mainPane, 1400, 900); // resize the window
 		mainStage.setScene(mainScene);
                 mainScene.getStylesheets().add("style.css");
 		reloadQues();
@@ -272,13 +289,14 @@ public class Main extends Application {
 		});
 		winResult = new ResultForm();
 
-		// Playing Siren Sound when time is less than 10 seconds
+		// Fetch and play the Siren Sound when time is less than 10 seconds
 		String musicFile = "./data/Sound/sirene.mp3";
 		Media sound = new Media(new File(musicFile).toURI().toString());
 		mdPlayer = new MediaPlayer(sound);
 
 	}
 
+        // method to get contestants photo and display on the right segment of quiz screen
 	private void ContestantPhoto() {
 		FileInputStream input1;
 		try {
@@ -296,7 +314,7 @@ public class Main extends Application {
 		
 	}
 
-	// Display the country flag at the bottom right of the test screen
+	// Display the country flag at the bottom right of the quiz screen
 	public void flag() {
 		FileInputStream input = null;
 		try {
@@ -313,7 +331,7 @@ public class Main extends Application {
 		mainPane.getChildren().add(imageFlag);
 	}
 
-	// Method for timer
+	// method to handle the timer starting at 5 minutes and giving both audible and visual warning at 10 seconds remaining.
 	public void startTiming(Stage mainStage) {
 		if (timeline != null) {
 			timeline.stop();
@@ -325,11 +343,11 @@ public class Main extends Application {
 			timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(1), event -> {
 				timeSeconds--;
 				timerLabel.setText(timeSeconds.toString() + " seconds");
-				if (timeSeconds <= 10) {
+				if (timeSeconds <= 10) { // condition to check if 10 seconds remaining
 					timerLabelR.setText(timeSeconds.toString() + " seconds");
 					mdPlayer.play();
 				}
-				if (timeSeconds <= 0) {
+				if (timeSeconds <= 0) { // condition to check if the timer has been over
 					mdPlayer.pause();
 					timeline.stop();
 					mainStage.hide();
@@ -342,21 +360,22 @@ public class Main extends Application {
 		}
 	}
 
-	// Reloading of questions
+	// Reloading the questions
 	public void reloadQues() {
-		labQuesNo.setText("Question " + Integer.toString(activeQ) + " of " + totQues);
-		labQues.setText(quesList.get(activeQ - 1).getTheQues());
-		radChoice1.setText("  " + quesList.get(activeQ - 1).getChoice(0));
-		radChoice2.setText("  " + quesList.get(activeQ - 1).getChoice(1));
-		radChoice3.setText("  " + quesList.get(activeQ - 1).getChoice(2));
-		radChoice4.setText("  " + quesList.get(activeQ - 1).getChoice(3));
+		labQuesNo.setText("Question " + Integer.toString(activeQues) + " of " + totQues);
+		labQues.setText(quesList.get(activeQues - 1).getTheQues());
+		radChoice1.setText("  " + quesList.get(activeQues - 1).getChoice(0));
+		radChoice2.setText("  " + quesList.get(activeQues - 1).getChoice(1));
+		radChoice3.setText("  " + quesList.get(activeQues - 1).getChoice(2));
+		radChoice4.setText("  " + quesList.get(activeQues - 1).getChoice(3));
 		imgQues.setImage(null);
 		imgAnsA.setImage(null);
 		imgAnsB.setImage(null);
 		imgAnsC.setImage(null);
 		imgAnsD.setImage(null);
 
-		if (quesList.get(activeQ - 1).getType() == 1) {
+		if (quesList.get(activeQues - 1).getType() == 1) { // condition for checking the type of question if its type A
+                    // Setting the layout for the text choice of type A questions
 			labA.setLayoutX(25);
 			labA.setLayoutY(75);
 			radChoice1.setLayoutX(50);
@@ -374,9 +393,10 @@ public class Main extends Application {
 			radChoice4.setLayoutX(50);
 			radChoice4.setLayoutY(232);
 		}
-		if (quesList.get(activeQ - 1).getType() == 2) {
-			File pFile = new File("data/QuestionsPics/" + quesList.get(activeQ - 1).getQuesPic());
+		if (quesList.get(activeQues - 1).getType() == 2) { // condition for checking the type of question if its type B
+			File pFile = new File("data/QuestionsPics/" + quesList.get(activeQues - 1).getQuesPic());
 			Image img = new Image(pFile.toURI().toString());
+                        // Setting the layout for the question image as well as radio buttons and labels for answers
 			imgQues.setImage(img);
 			imgAnsA.setImage(null);
 			imgAnsB.setImage(null);
@@ -399,26 +419,26 @@ public class Main extends Application {
 			radChoice4.setLayoutX(50);
 			radChoice4.setLayoutY(492);
 		}
-		radChoice1.setSelected(quesList.get(activeQ - 1).getSelected(0));
-		radChoice2.setSelected(quesList.get(activeQ - 1).getSelected(1));
-		radChoice3.setSelected(quesList.get(activeQ - 1).getSelected(2));
-		radChoice4.setSelected(quesList.get(activeQ - 1).getSelected(3));
+		radChoice1.setSelected(quesList.get(activeQues - 1).getSelected(0));
+		radChoice2.setSelected(quesList.get(activeQues - 1).getSelected(1));
+		radChoice3.setSelected(quesList.get(activeQues - 1).getSelected(2));
+		radChoice4.setSelected(quesList.get(activeQues - 1).getSelected(3));
 
-		if (quesList.get(activeQ - 1).getType() == 3) {
-			File pFile1 = new File("data/QuestionsPics/" + quesList.get(activeQ - 1).getAnsPicA());
+		if (quesList.get(activeQues - 1).getType() == 3) { // condition for checking the type of question if its type 
+			File pFile1 = new File("data/QuestionsPics/" + quesList.get(activeQues - 1).getAnsPicA());
 			Image imgA = new Image(pFile1.toURI().toString());
-			File pFile2 = new File("data/QuestionsPics/" + quesList.get(activeQ - 1).getAnsPicB());
+			File pFile2 = new File("data/QuestionsPics/" + quesList.get(activeQues - 1).getAnsPicB());
 			Image imgB = new Image(pFile2.toURI().toString());
-			File pFile3 = new File("data/QuestionsPics/" + quesList.get(activeQ - 1).getAnsPicC());
+			File pFile3 = new File("data/QuestionsPics/" + quesList.get(activeQues - 1).getAnsPicC());
 			Image imgC = new Image(pFile3.toURI().toString());
-			File pFile4 = new File("data/QuestionsPics/" + quesList.get(activeQ - 1).getAnsPicD());
+			File pFile4 = new File("data/QuestionsPics/" + quesList.get(activeQues - 1).getAnsPicD());
 			Image imgD = new Image(pFile4.toURI().toString());
+                        // Setting the layout for the answer images as well as radio buttons
 			imgQues.setImage(null);
 			imgAnsA.setImage(imgA);
 			imgAnsB.setImage(imgB);
 			imgAnsC.setImage(imgC);
 			imgAnsD.setImage(imgD);
-
 			labA.setLayoutY(165);
 			radChoice1.setLayoutY(170);
 			labB.setLayoutY(385);
@@ -432,19 +452,19 @@ public class Main extends Application {
 			radChoice4.setLayoutX(475);
 			radChoice4.setLayoutY(390);
 		}
-		radChoice1.setSelected(quesList.get(activeQ - 1).getSelected(0));
-		radChoice2.setSelected(quesList.get(activeQ - 1).getSelected(1));
-		radChoice3.setSelected(quesList.get(activeQ - 1).getSelected(2));
-		radChoice4.setSelected(quesList.get(activeQ - 1).getSelected(3));
+		radChoice1.setSelected(quesList.get(activeQues - 1).getSelected(0));
+		radChoice2.setSelected(quesList.get(activeQues - 1).getSelected(1));
+		radChoice3.setSelected(quesList.get(activeQues - 1).getSelected(2));
+		radChoice4.setSelected(quesList.get(activeQues - 1).getSelected(3));
 	}
 
-	// Save Contestant's Answer to answers.txt
+	// method to save contestant's Answer to file "answers.txt"
 	public void saveToFile() {
 		try {
 			String selected = "";
 			PrintWriter pw = new PrintWriter(new FileWriter(myf2, true));
 			for (TestForm i : quesList) {
-				if (i.getSelected(0)) {
+				if (i.getSelected(0)) { 
 					selected = "A";
 				} else if (i.getSelected(1)) {
 					selected = "B";
@@ -453,7 +473,7 @@ public class Main extends Application {
 				} else if (i.getSelected(3)) {
 					selected = "D";
 				} else {
-					selected = "N";
+					selected = "N"; // if no choice is selected then N for default is selected
 				}
 				pw.print(selected + ":");
 			}
@@ -465,7 +485,7 @@ public class Main extends Application {
 		}
 	}
 
-	// Read Questions from inputdata.txt
+	// method to read questions from the file "input.txt"
 	public void readFromFile() {
 		Scanner sfile;
 		int type;
@@ -483,10 +503,10 @@ public class Main extends Application {
 			String aLine = sfile.nextLine();
 			Scanner sline = new Scanner(aLine);
 			totQues = Integer.parseInt(sline.next());
-			for (int k = 1; k <= totQues; k++) {
+			for (int k = 1; k <= totQues; k++) { // condition to loop through all questions
 				aLine = sfile.nextLine();
 				sline = new Scanner(aLine);
-				sline.useDelimiter(":");
+				sline.useDelimiter(":"); // seperation by a colon
 				type = Integer.parseInt(sline.next());
 				answer = sline.next().charAt(0);
 				theQues = sline.next();
@@ -494,16 +514,16 @@ public class Main extends Application {
 				choices[1] = "";
 				choices[2] = "";
 				choices[3] = "";
-				if (type == 2) {
+				if (type == 2) { // condition to check the type of question
 					quesPic = sline.next();
 				}
-				if (type == 3) {
+				if (type == 3) { // Layout and styling for choice A label
 					ansPic1 = sline.next();
 					ansPic2 = sline.next();
 					ansPic3 = sline.next();
 					ansPic4 = sline.next();
 				}
-				if (type == 1 || type == 2) {
+				if (type == 1 || type == 2) { // Layout and styling for choice A label
 					choices[0] = sline.next();
 					choices[1] = sline.next();
 					choices[2] = sline.next();
